9e099c34190e2252cca84e71576a371fecaa4e3b
terrablue <102580937+terrablue@users.noreply.github.com>
2022-04-20 10:56:36 +0200

    update exports location

---
+++ exports.js
@@ -0,0 +1,28 @@
+import conf from "./source/conf.js";
+import App from "./source/App.js";
+
+export {App};
+export {default as Bundler} from "./source/Bundler.js";
+export {default as EagerPromise, eager} from "./source/EagerPromise.js" ;
+
+export {default as Domain} from "./source/domain/Domain.js";
+export {default as Storeable} from "./source/types/Storeable.js";
+
+export * from "./source/errors.js";
+export * from "./source/invariants.js";
+
+export {default as MemoryStore} from "./source/store/Memory.js";
+export {default as Store} from "./source/store/Store.js";
+
+export {default as extend_object} from "./source/extend_object.js";
+export {default as sanitize} from "./source/sanitize.js";
+
+export {default as html} from "./source/handlers/html.js";
+export {default as json} from "./source/handlers/json.js";
+export {default as redirect} from "./source/handlers/redirect.js";
+
+export {default as router} from "./source/Router.js";
+
+const app = new App(conf());
+
+export {app};
