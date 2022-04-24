916b1349d77df73c8b60202758142c047849ef55
terrablue <102580937+terrablue@users.noreply.github.com>
2022-04-24 21:34:23 +0200

    feat(routing): map named capture groups to request.path.<name>

--- source/Router.js
+++ source/Router.js
@@ -6,8 +6,8 @@ const dealias = path => aliases.reduce((dealiased, {key, value}) =>
   dealiased.replace(key, () => value), path);
 const push = (type, path, handler) =>
   routes.push({type, "path": new RegExp(`^${dealias(path)}$`, "u"), handler});
-const find = (type, path, fallback) => routes.find(route =>
-  route.type === type && route.path.test(path))?.handler ?? fallback;
+const find = (type, path, fallback = {"handler": r => r}) => routes.find(route =>
+  route.type === type && route.path.test(path)) ?? fallback;
 
 export default {
   "map": (path, callback) => push("map", path, callback),
@@ -19,10 +19,13 @@ export default {
     const url = new URL(`https://primatejs.com${original_request.pathname}`);
     const {pathname, searchParams} = url;
     const params = Object.fromEntries(searchParams);
-    const request = {...original_request, pathname, params,
-      "path": pathname.split("/").filter(path => path !== ""),
-    };
-    const verb = find(method, pathname, () => http404``);
-    return verb(await find("map", pathname, _ => _)(request));
+    const verb = find(method, pathname, () => ({"handler": http404``}));
+    const path = pathname.split("/").filter(path => path !== "");
+    Object.entries(verb.path.exec(pathname)?.groups ?? [])
+      .filter(([key]) => path[key] === undefined)
+      .forEach(([key, value]) => Object.defineProperty(path, key, {value}));
+
+    const request = {...original_request, pathname, params, path};
+    return verb.handler(await find("map", pathname).handler(request));
   },
 };
