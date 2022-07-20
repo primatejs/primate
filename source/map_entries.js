c792b2791339f1be886f0e67447c6d54c480e013
terrablue <102580937+terrablue@users.noreply.github.com>
2022-07-20 14:26:03 +0200

    remove map_entries

--- source/map_entries.js
+++
@@ -1,6 +0,0 @@
-import {is, invariant} from "./invariants.js";
-
-export default (object = {}, mapper = (...args) => args) =>
-  invariant(() => is.object(object) && is.function(mapper))
-  && Object.fromEntries(Object.entries(object).map(([key, value]) =>
-    mapper(key, value)));
--- test/fixtures/map_entries.js
+++
@@ -1,2 +0,0 @@
-import map_entries from "../../source/map_entries.js";
-export default () => map_entries;
