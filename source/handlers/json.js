c43ee3727e36e2a547b6c3f5ad1f4c34b474b93d
Codye Watson <graphettion@gmail.com>
2022-08-29 11:52:52 -0500

    Return data instead of handler from routes

--- source/handlers/json.js
+++ source/handlers/json.js
@@ -2,5 +2,6 @@ export default async (strings, ...keys) => {
   const body = JSON.stringify(await keys[0]);
   const code = 200;
   const headers = {"Content-Type": "application/json"};
-  return {body, code, headers};
+  const type = Symbol.for("handler");
+  return {body, code, headers, type};
 };
