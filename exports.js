7daa9b7dceb8b6af8d2630b737c6d267046620eb
terrablue <102580937+terrablue@users.noreply.github.com>
2023-01-07 01:05:34 +0200

    clean up exports

--- exports.js
+++ exports.js
@@ -1,10 +1,5 @@
 import {File, Path} from "runtime-compat/filesystem";
 
-import _conf from "./src/conf.js";
-const conf = _conf();
-import App from "./src/App.js";
-
-export {App};
 export {default as Bundler} from "./src/Bundler.js";
 
 export {default as Domain} from "./src/domain/Domain.js";
@@ -17,10 +12,12 @@ export {default as Store} from "./src/store/Store.js";
 
 export {default as extend_object} from "./src/extend_object.js";
 
+import _conf from "./src/conf.js";
+const conf = _conf();
 import _html from "./src/handlers/html.js";
 const {paths: {components: path}} = conf;
-const loadFile = async file => [new Path(file.path).base, await file.read()];
-const components = await path.file.exists
+const loadFile = async file => [file.base, await file.read()];
+const components = await path.exists
   ? Object.fromEntries(await Promise.all((
     await File.collect(path, ".html")).map(loadFile)))
   : {};
@@ -31,7 +28,3 @@ export {default as redirect} from "./src/handlers/redirect.js";
 export {http404} from "./src/handlers/http.js";
 
 export {default as DOMParser} from "./src/handlers/DOM/Parser.js";
-
-const app = new App(conf);
-
-export {app};
