2f98a398726c28eaa9bad5846a8724924b369247
terrablue <102580937+terrablue@users.noreply.github.com>
2023-04-12 14:10:01 +0300

    clean up old structures

--- src/errors.js
+++
@@ -1,2 +0,0 @@
-export {default as InternalServerError} from "./errors/InternalServer.js";
-export {default as PredicateError} from "./errors/Predicate.js";
--- src/errors/Predicate.js
+++
@@ -1,1 +0,0 @@
-export default class PredicateError extends Error {}
