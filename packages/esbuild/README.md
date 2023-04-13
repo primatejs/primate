067473babe3dd9c67aefd17a065feb2caee0af33
terrablue <102580937+terrablue@users.noreply.github.com>
2023-04-13 16:14:57 +0300

    add @primate/esbuild

---
+++ packages/esbuild/README.md
@@ -0,0 +1,17 @@
+# Primate esbuild module
+
+## Install
+
+`npm i @primate/esbuild`
+
+## Configure
+
+Add to `primate.config.js`
+
+```js
+import esbuild from "@primate/esbuild";
+
+export default {
+  modules: [esbuild()],
+};
+```
---
+++ packages/esbuild/src/module.js
@@ -0,0 +1,50 @@
+import esbuild from "esbuild";
+
+export default () => ({
+  async bundle(app, next) {
+    const build = app.root.join("build");
+    if (await build.exists) {
+      app.log.warn("build folder already exists, bundling disabled");
+      return next(app);
+    }
+    await build.file.create();
+    // write all the published resources to the build directory
+    for (const resource of app.resources) {
+      const {src, code} = resource;
+      const filePath = src.split("/");
+      const dirPart = filePath.slice(0, -1);
+      const directory = dirPart.length > 0 ? build.join(...dirPart) : build;
+      const file = directory.join(...filePath.slice(-1));
+      if (!await directory.exists) {
+        await directory.file.create();
+      }
+      await file.file.write(code);
+    }
+
+    // only bundle if there are entrypoints
+    if (app.entrypoints.length > 0) {
+      // clear memory resources
+      while (app.resources.length > 0) {
+        app.resources.pop();
+      }
+      const dist = app.entrypoints.map(({code}) => code).join("");
+      // construct new dist
+      const {outputFiles: files} = await esbuild.build({
+        stdin: {
+          contents: dist,
+          resolveDir: `${build}`,
+        },
+        bundle: true,
+        minify: true,
+        write: false,
+        format: "esm",
+        outdir: `${build}`,
+        logLevel: app.debug ? "warning" : "error",
+      });
+      await app.publish({src: "app.js", code: files[0].text, type: "module"});
+      await app.publish({src: "app.css", code: files[1].text, type: "style"});
+    }
+    await build.file.remove();
+    return next(app);
+  },
+});
