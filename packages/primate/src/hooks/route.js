fc5be4974bef0614e228bc65aede273d0c8f8daf
terrablue <102580937+terrablue@users.noreply.github.com>
2023-04-16 13:14:20 +0300

    core: rework routing code

--- packages/primate/src/hooks/route.js
+++ packages/primate/src/hooks/route.js
@@ -12,19 +12,47 @@ const verbs = [
   "delete", "connect", "options", "trace", "patch",
 ];
 
+const toRoute = file => {
+  const ending = -3;
+  const route = file
+    // remove ending
+    .slice(0, ending)
+    // transform /index -> ""
+    .replace("/index", "")
+    // transform index -> ""
+    .replace("index", "")
+    // prepare for regex
+    .replaceAll(/\{(?<named>.*)\}/gu, (_, name) => `(?<${name}>.*?)`)
+  ;
+  return new RegExp(`^/${route}$`, "u");
+};
+
 export default async app => {
-  const routes = [];
-  const find = (method, path, fallback = {handler: r => r}) =>
-    routes.find(route =>
-      ieq(route.method, method) && route.path.test(path)) ?? fallback;
+  const routes = (await Promise.all(
+    (await Path.collect(app.paths.routes, /^.*.js$/u))
+      .map(async route => {
+        const imported = (await import(route)).default;
+        const file = `${route}`.replace(app.paths.routes, "").slice(1);
+        if (imported === undefined) {
+          app.log.warn(`empty route file at ${file}`);
+          return [];
+        }
+
+        const path = toRoute(file);
+        return Object.entries(imported)
+          .filter(([verb]) => verbs.includes(verb))
+          .map(([method, handler]) => ({method, handler, path}));
+      }))).flat();
+  const find = (method, path) => routes.find(route =>
+    ieq(route.method, method) && route.path.test(path));
 
   const router = {
-    async route({request, url, app, ...rest}) {
+    async route({request, url, ...rest}) {
       const {method} = request;
       const {pathname, searchParams} = url;
-      const verb = find(method, pathname, {handler: () => {
+      const verb = find(method, pathname) ?? (() => {
         throw new Logger.Warn(`no ${method} route to ${pathname}`);
-      }});
+      })();
 
       const data = {
         request,
@@ -34,35 +62,8 @@ export default async app => {
         ...rest,
       };
 
-      return verb.handler(await find("map", pathname).handler(data));
+      return verb.handler(data);
     },
   };
-  const toRoute = file => {
-    const ending = -3;
-    const route = file
-      // remove ending
-      .slice(0, ending)
-      // transform /index -> ""
-      .replace("/index", "")
-      // transform index -> ""
-      .replace("index", "")
-      // prepare for regex
-      .replaceAll(/\{(?<named>.*)\}/gu, (_, name) => `(?<${name}>.*?)`)
-    ;
-    return new RegExp(`^/${route}$`, "u");
-  };
-  for (const route of await Path.collect(app.paths.routes, /^.*.js$/u)) {
-    const imported = (await import(route)).default;
-    const file = `${route}`.replace(app.paths.routes, "").slice(1);
-    if (imported === undefined) {
-      app.log.warn(`empty route file at ${file}`);
-    } else {
-      const valids = Object.entries(imported)
-        .filter(([verb]) => verbs.includes(verb));
-      for (const [method, handler] of valids) {
-        routes.push({method, path: toRoute(file), handler});
-      }
-    }
-  }
   return router;
 };
