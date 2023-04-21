6fb74a1ab1408c9e4fb4e6a3853720979960183b
terrablue <102580937+terrablue@users.noreply.github.com>
2023-04-21 18:38:46 +0300

    expose URL and Response from runtime-compat

--- packages/primate/src/exports.js
+++ packages/primate/src/exports.js
@@ -1,5 +1,9 @@
 import run from "./run.js";
 
 export * from "./handlers/exports.js";
+
 export {default as Logger} from "./Logger.js";
+
+export {URL, Response} from "runtime-compat/http";
+
 export default command => run(command);
