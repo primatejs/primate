9c185e1817bb69214194c5d47f296da67c7016ba
terrablue <102580937+terrablue@users.noreply.github.com>
2023-05-05 01:20:50 +0300

    validation: fix exports

--- packages/validation/src/types/exports.js
+++ packages/validation/src/types/exports.js
@@ -1,4 +1,4 @@
+export {default as Int16} from "./Int16.js";
 export {default as Int32} from "./Int32.js";
 export {default as Int64} from "./Int64.js";
-export {default as Double} from "./Double.js";
 export {default as UUID} from "./UUID.js";
