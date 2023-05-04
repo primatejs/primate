1ac37c6ca22cda07429f60b5c87c473775895f73
terrablue <102580937+terrablue@users.noreply.github.com>
2023-05-04 22:43:29 +0300

    store: add base types, add JSON, BigInt

--- packages/store/src/types.js
+++ packages/store/src/types.js
@@ -4,19 +4,45 @@ export default {
   [String]: {
     validate: value => typeof value === "string",
     message: "Must be a valid string",
+    base: "string",
   },
   [Number]: {
     coerce: value => numeric(value) ? Number(value) : value,
     validate: value => typeof value === "number",
     message: "Must be a valid number",
+    base: "float",
   },
   [Boolean]: {
     coerce: value => boolish(value) ? value === "true" : value,
     validate: value => typeof value === "boolean",
     message: "Must be true or false",
+    base: "boolean",
   },
   [Date]: {
     validate: value => value instanceof Date,
     message: "Must be a valid date",
+    base: "datetime",
+  },
+  [JSON]: {
+    validate: value => {
+      try {
+        JSON.stringify(value);
+        return true;
+      } catch (_) {
+        return false;
+      }
+    },
+    base: "json",
+  },
+  [BigInt]: {
+    validate: value => typeof value === "bigint",
+    coerce: value => {
+      try {
+        return BigInt(value);
+      } catch (_) {
+        return value;
+      }
+    },
+    base: "bigint",
   },
 };
