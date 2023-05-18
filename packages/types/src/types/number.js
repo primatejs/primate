6ffdd9122702e96feb11679b78d4263213341853
terrablue <102580937+terrablue@users.noreply.github.com>
2023-05-19 00:30:24 +0300

    types: fix number

--- packages/types/src/types/number.js
+++ packages/types/src/types/number.js
@@ -1,31 +1,44 @@
 import {numeric} from "runtime-compat/dyndef";
 import {range} from "./predicates/exports.js";
 
-const min = Number.MIX_SAFE_INTEGER;
-const max = Number.MAX_SAFE_INTEGER;
-
 const coercibles = {
   string: value => numeric(value) ? Number(value) : value,
   number: value => value,
   bigint: value => Number(value),
 };
 
-export const base = "double";
+const bounds = {
+  min: Number.MIN_SAFE_INTEGER,
+  max: Number.MAX_SAFE_INTEGER,
+};
+
+const ntype = value => typeof value === "number";
+const nrange = range(bounds.min, bounds.max);
+
+const coerce = value => coercibles[typeof value]?.(value) ?? value;
 
 const number = value => {
-  const coerced = coercibles[typeof value]?.(value) ?? value;
-  if (typeof coerced === "number" && range(coerced, min, max)) {
+  const coerced = coerce(value);
+  if (typeof coerced === "number" && nrange(coerced)) {
     return coerced;
   }
   throw new Error(`${value} is not a number`);
 };
 
-number.range = (value, min, max) => {
-  const coerced = coercibles[typeof value]?.(value) ?? value;
-  if (range(coerced, min, max)) {
-    return value;
+number.range = (min, max) => {
+  if (min < bounds.min || max > bounds.max) {
+    throw new Error(`number range ${min}-${max} not within safe integer range`);
   }
-  throw new Error(`${value} is not in the range of ${min} of ${max}`);
+  return value => {
+    const customRange = range(min, max);
+    const coerced = coerce(value);
+    if (typeof coerced === "number" && customRange(coerced)) {
+      return value;
+    }
+    throw new Error(`${value} is not in the range of ${min} of ${max}`);
+  };
 };
 
 export default number;
+
+export const base = "float";
