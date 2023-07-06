ccc65269b636729f7071c425fe5a2ef90876740e
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-06 13:27:58 +0300

    change runtime type functions from x into the getX form

--- packages/primate/src/dispatch.js
+++ packages/primate/src/dispatch.js
@@ -1,12 +1,13 @@
 import {is, maybe} from "runtime-compat/dyndef";
 import {tryreturn} from "runtime-compat/sync";
 import {map} from "runtime-compat/object";
+import {camelcased} from "runtime-compat/string";
 import errors from "./errors.js";
 
 export default (patches = {}) => value => {
   is(patches.get).undefined();
   return Object.assign(Object.create(null), {
-    ...map(patches, ([name, patch]) => [name, property => {
+    ...map(patches, ([name, patch]) => [`get${camelcased(name)}`, property => {
       is(property).defined(`\`${name}\` called without property`);
       return tryreturn(_ => patch(value[property], property))
         .orelse(({message}) => errors.MismatchedType.throw(message));
