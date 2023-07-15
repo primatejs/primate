c3cce65772c50da3aefc348a8ae66a06822ac271
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-15 03:31:05 +0300

    types: 0.4.1

--- packages/types/src/native/string.js
+++ packages/types/src/native/string.js
@@ -1,10 +1,9 @@
-export const type = "string";
-
-const string = value => {
-  if (typeof value === "string") {
-    return value;
-  }
-  throw new Error(`${value} is not a string`);
+export default {
+  base: "string",
+  type(value) {
+    if (typeof value === "string") {
+      return value;
+    }
+    throw new Error(`\`${value}\` is not a string`);
+  },
 };
-
-export default string;
