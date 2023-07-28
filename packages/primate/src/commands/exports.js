c097fdd2091135afb9033e19b91991a2025d52d2
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-28 21:26:59 +0300

    simplify: commands/exports

--- packages/primate/src/commands/exports.js
+++ packages/primate/src/commands/exports.js
@@ -1,8 +1,4 @@
 import {default as dev} from "./dev.js";
 import {default as serve} from "./serve.js";
 
-const commands = {dev, serve};
-
-const run = name => commands[name] ?? dev;
-
-export default name => name === undefined ? dev : run(name);
+export default name => ({dev, serve})[name] ?? dev;
