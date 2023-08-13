85de18ea6f61a2bfd8abad055f6e33275d4e8c74
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-13 22:22:20 +0300

    use the return value of cascade

--- packages/primate/src/start.js
+++ packages/primate/src/start.js
@@ -2,14 +2,14 @@ import {serve, Response, Status} from "runtime-compat/http";
 import {cascade, tryreturn} from "runtime-compat/async";
 import * as hooks from "./hooks/exports.js";
 
-export default async (app, deactivated = []) => {
+export default async (app$, deactivated = []) => {
   // run one-time hooks
-  await cascade(["init", "register", "compile", "publish", "bundle"]
+  const app = await cascade(["init", "register", "compile", "publish", "bundle"]
     .filter(hook => !deactivated.includes(hook))
     .map(hook => async (input, next) => {
-      app.log.info(`running ${hook} hooks`, {module: "primate"});
+      app$.log.info(`running ${hook} hooks`, {module: "primate"});
       return next(await hooks[hook](input));
-    }))(app);
+    }))(app$);
 
   const server = await serve(async request =>
     tryreturn(async _ => hooks.handle(app)(await app.parse(request)))
