9d44fb4331500fc904f01ab6c76c075ebdfd76cf
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-18 18:50:16 +0300

    solid: adapt to new api

--- packages/solid/src/module.js
+++ packages/solid/src/module.js
@@ -1,16 +1,14 @@
 import crypto from "runtime-compat/crypto";
 import {Path} from "runtime-compat/fs";
 import {Response, Status, MediaType} from "runtime-compat/http";
+import {tryreturn} from "runtime-compat/async";
+
+import {renderToString as render} from "solid-js/web";
 import {transformAsync} from "@babel/core";
 import solid from "babel-preset-solid";
-import {renderToString} from "solid-js/web";
-import {tryreturn} from "runtime-compat/async";
+
 import errors from "./errors.js";
 import {client, create_root, rootname, hydrate} from "./client/exports.js";
-
-// do not hard-depend on node
-const packager = import.meta.runtime?.packager ?? "package.json";
-const library = import.meta.runtime?.library ?? "node_modules";
 
 const encoder = new TextEncoder();
 const hash = async (string, algorithm = "sha-256") => {
@@ -26,18 +24,19 @@ const hash = async (string, algorithm = "sha-256") => {
 const normalize = async path => `solid_${await hash(path)}`;
 
 const import$ = async (module, app, copy_dependency) => {
-  const {build: {modules}, http: {static: {root}}} = app.config;
+  const {library, packager} = app;
+  const {http: {static: {root}}} = app.config;
   const parts = module.split("/");
   const path = [library, ...parts];
   const pkg = await Path.resolve().join(...path, packager).json();
   if (copy_dependency) {
     const dependency = Path.resolve().join(...path);
-    const to = app.build.paths.client.join(modules, ...parts);
+    const to = app.build.paths.client.join(library, ...parts);
     await dependency.file.copy(`${to}`);
   }
 
   const entry = pkg.exports["."].browser.import;
-  app.importmaps[module] = new Path(root, modules, module, entry).path;
+  app.importmaps[module] = new Path(root, library, module, entry).path;
 };
 
 const filename = `${rootname}.js`;
@@ -50,14 +49,14 @@ const load = async path =>
 const make_component = base => async (name, props) =>
   ({name, component: await load(base.join(name)), props});
 
-const handler = (name, props = {}, {status = Status.OK} = {}) =>
+const handler = (name, props = {}, {status = Status.OK, page} = {}) =>
   async (app, {layouts = [], as_layout} = {}, request) => {
     const options = {
       liveview: app.liveview !== undefined,
     };
     const {headers} = request;
     const {paths} = app.build;
-    const make = make_component(paths.server.join(app.config.build.app));
+    const make = make_component(paths.server.join(app.config.paths.components));
     if (as_layout) {
       return make(name, props);
     }
@@ -80,7 +79,7 @@ const handler = (name, props = {}, {status = Status.OK} = {}) =>
 
     const root = paths.server.join(filename);
     const imported = (await import(root)).default;
-    const body = renderToString(() => imported({
+    const body = render(() => imported({
       components: components.map(({component}) => component),
       data,
     }));
@@ -91,7 +90,7 @@ const handler = (name, props = {}, {status = Status.OK} = {}) =>
     // needs to be called before app.render
     const headers$ = await app.headers();
 
-    return new Response(await app.render({body}), {
+    return new Response(await app.render({body, page}), {
       status,
       headers: {...headers$, "Content-Type": MediaType.TEXT_HTML},
     });
@@ -130,13 +129,12 @@ export default ({
   };
 
   return {
-    name: "@primate/solid",
-    init(app, next/*0.21 compat, remove >0.21*/ = () => null) {
+    name: "primate:solid",
+    init(app, next) {
       env.source = app.build.paths.components;
-
       return next(app);
     },
-    register(app, next/*0.21 compat, remove >0.21*/ = () => null) {
+    register(app, next) {
       app.register(extension, handler);
       return next(app);
     },
@@ -144,7 +142,7 @@ export default ({
       // copy components to build/components
       await app.copy(app.paths.components, env.source, copy_re);
       const components = await env.source.collect(collect_re);
-      const target = app.build.paths.server.join(app.config.build.app);
+      const target = app.build.paths.server.join(app.config.paths.components);
       await target.file.create();
 
       await Promise.all(components.map(async component => {
@@ -155,7 +153,7 @@ export default ({
         await to.file.write(code.replaceAll(extensions.from, extensions.to));
       }));
 
-      const root = await transform.server(create_root(app.layoutDepth,
+      const root = await transform.server(create_root(app.layout.depth,
         dynamicProps));
       const to = app.build.paths.server.join(filename);
       await to.file.write(root);
@@ -163,47 +161,47 @@ export default ({
       return next(app);
     },
     async publish(app, next) {
-      const target = app.build.paths.client.join(app.config.build.app);
-      await target.file.create();
+      // import libs
+      await import$("solid-js", app, true);
+      await import$("solid-js/web", app);
 
       // create client components
       const components = await env.source.collect(collect_re);
+
       await Promise.all(components.map(async component => {
-        const name = component.path.replace(`${env.source}/`, "");
+        const name = component.path.replace(`${env.source}/`, _ => "");
         const file = await component.file.read();
         const code = await transform.client(file);
-        const build = app.config.build.app;
+        const build = app.config.paths.components;
         const {path} = component;
+        const file_string = `./${build}/${name}`;
 
         {
-          {
-            const src = `${path}.js`.replace(`${env.source}`, _ => build);
-            await app.publish({src,
-              code: code.replaceAll(extensions.from, extensions.to),
-            type});
-          }
+          const src = `${path}.js`.replace(`${env.source}`, _ => build);
+          await app.publish({
+            src,
+            code: code.replaceAll(extensions.from, extensions.to),
+            type,
+          });
           const imported = await normalize(name);
-          app.bootstrap({
+          app.export({
             type: "script",
-            code: `export {default as ${imported}} from "./${name}.js";\n`,
+            code: `export {default as ${imported}} from "${file_string}.js";\n`,
           });
         }
       }));
 
-      await import$("solid-js", app, true);
-      await import$("solid-js/web", app);
-
-      app.bootstrap({type: "script", code: hydrate});
+      app.export({type: "script", code: hydrate});
 
       {
-        const code = await transform.client(create_root(app.layoutDepth,
+        const code = await transform.client(create_root(app.layout.depth,
           dynamicProps));
         const src = new Path(app.config.http.static.root, filename);
         await app.publish({src, code, type});
       }
       {
-        const code = `export {default as ${rootname}} from "../${filename}";\n`;
-        app.bootstrap({type: "script", code});
+        const code = `export {default as ${rootname}} from "./${filename}";\n`;
+        app.export({type: "script", code});
       }
 
       return next(app);
