507dad36f6351d1481ccdc8752d419ea9fb23aef
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-18 20:55:17 +0300

    vue: adapt to new api

--- packages/vue/src/module.js
+++ packages/vue/src/module.js
@@ -1,54 +1,78 @@
+import {File} from "runtime-compat/fs";
 import {Response, Status, MediaType} from "runtime-compat/http";
+import {tryreturn} from "runtime-compat/async";
+
 import {createSSRApp} from "vue";
 import {renderToString} from "vue/server-renderer";
-import {parse} from "vue/compiler-sfc";
+import {parse, compileTemplate} from "vue/compiler-sfc";
 
-const render = (template, props) => {
-  const app = createSSRApp({data: () => props, template});
-  return renderToString(app);
+import errors from "./errors.js";
+
+const load = async path =>
+  tryreturn(_ => import(`${path}.js`))
+    .orelse(_ => errors.MissingComponent.throw(path.name, path));
+
+const make_component = base => async (name, props) =>
+  ({name, component: await load(base.join(name)), props});
+
+const handler = (name, props = {}, {status = Status.OK, page} = {}) =>
+  async app => {
+    const {paths} = app.build;
+    const make = make_component(paths.server.join(app.config.paths.components));
+
+    const imported = await make(name, props);
+    const component = createSSRApp({
+      render: imported.component.render,
+      data: () => props,
+    });
+    const body = await renderToString(component);
+
+    return new Response(await app.render({body, page}), {
+      status,
+      headers: {...app.headers(), "Content-Type": MediaType.TEXT_HTML},
+    });
+  };
+
+export default ({
+  extension = "vue",
+} = {}) => {
+  const env = {};
+  const copy_re = new RegExp(`^.*.(?:${extension})$`, "u");
+  const collect_re = new RegExp(`^.*.${extension}$`, "u");
+  const extensions = {
+    from: `.${extension}`,
+    to: `.${extension}.js`,
+  };
+
+  return {
+    name: "primate:vue",
+    init(app, next) {
+      env.source = app.build.paths.components;
+      return next(app);
+    },
+    register(app, next) {
+      app.register(extension, handler);
+      return next(app);
+    },
+    async compile(app, next) {
+      // create client components
+      await app.copy(app.paths.components, env.source, copy_re);
+      const components = await env.source.collect(collect_re);
+      const target = app.build.paths.server.join(app.config.paths.components);
+      await target.file.create();
+
+      await Promise.all(components.map(async component => {
+        const file = await component.file.read();
+        const {descriptor: {template: {content}}} = parse(file);
+        const {code} = compileTemplate({
+          source: content,
+          id: "1",
+        });
+        const to = target.join(`${component.path}.js`.replace(env.source, ""));
+        await to.file.write(code.replaceAll(extensions.from, extensions.to));
+      }));
+
+      return next(app);
+    },
+  };
 };
-
-const handler = (name, props = {}, {status = Status.OK} = {}) => async app => {
-  const {build, config} = app;
-  const target = build.paths.server.join(config.build.app).join(`${name}.js`);
-  const body = await render(await target.text(), props);
-
-  return new Response(await app.render({body}), {
-    status,
-    headers: {...app.headers(), "Content-Type": MediaType.TEXT_HTML},
-  });
-};
-
-const vue = ".vue";
-export default ({dynamicProps = "data"} = {}) => ({
-  name: "primate:vue",
-  register(app, next) {
-    app.register("vue", handler);
-    return next(app);
-  },
-  async compile(app, next) {
-    const source = app.build.paths.components;
-    const target = app.build.paths.server.join(app.config.build.app);
-    await target.file.create();
-    const components = await source.list(filename => filename.endsWith(vue));
-    await Promise.all(components.map(async component => {
-      const file = await component.file.read();
-      const {descriptor} = parse(file);
-      const to = target.join(`${component.path}.js`.replace(source, ""));
-      await to.file.write(descriptor.template.content);
-    }));
-
-    return next(app);
-  },
-  /*async publish(app, next) {
-    const source = app.build.paths.components;
-    const target = app.build.paths.client.join(app.config.build.app);
-    await target.file.create();
-    const components = await source.list(filename => filename.endsWith(vue));
-    await Promise.all(components.map(async component => {
-      const file = await component.file.read();
-    }));
-
-    return next(app);
-  },*/
-});
