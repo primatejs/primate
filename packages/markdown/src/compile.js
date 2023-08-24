e58f6e3a5ff9e16099953c71705fbdd71e55e1de
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-24 18:51:31 +0300

    markdown: separate compile function

--- packages/markdown/src/compile.js
+++ packages/markdown/src/compile.js
@@ -1,39 +1,24 @@
 import {marked} from "marked";
 
 const identity_heading = (text, level) => `<h${level}>${text}</h${level}>`;
+const heading = options => options?.renderer?.heading ?? identity_heading;
 
-export default async ({
-  app,
-  directory,
-  options,
-}) => {
-  const {location} = app.config;
-  const monkeyed_heading = options?.renderer?.heading ?? identity_heading;
-  const source = app.runpath(directory);
-  const target = app.runpath(location.server, directory);
-  await target.file.create();
-
-  return async (filename, input) => {
-    const toc = [];
-    const renderer = {
-      ...options?.renderer ?? {},
-      heading(text, level) {
-        const name = text.toLowerCase().replaceAll(/[?{}%]/gu, "");
-        toc.push({level, text, name});
-        return monkeyed_heading(text, level);
-      },
-    };
-    marked.use({
-      ...options,
-      renderer,
-    });
-
-    const content = marked.parse(input);
-    const html = target.join(`${filename}.html`.replace(source, ""));
-    await html.directory.file.create();
-    await html.file.write(content);
-
-    const json = target.join(`${filename}.json`.replace(source, ""));
-    await json.file.write(JSON.stringify(toc));
+export default (input, options) => {
+  const toc = [];
+  const renderer = {
+    ...options?.renderer ?? {},
+    heading(text, level) {
+      toc.push({text, level,
+        name: text.toLowerCase()
+          .replaceAll(/[?{}%]/gu, "")
+          .replace(/[^\w]+/gu, "-"),
+      });
+      return heading(options)(text, level);
+    },
   };
+  marked.use({...options, renderer});
+
+  const content = marked.parse(input);
+
+  return {content, toc};
 };
--- packages/markdown/src/module.js
+++ packages/markdown/src/module.js
@@ -1,19 +1,18 @@
 import {Response, Status, MediaType} from "runtime-compat/http";
-import precompile from "./compile.js";
+import {stringify} from "runtime-compat/object";
+import compile from "./compile.js";
 
-const respond = (handler, directory) => (...[name, ...rest]) => async app => {
-  const base = app.runpath(app.config.location.server, directory);
-  const content = await base.join(`${name}.html`).text();
-  const toc = await base.join(`${name}.json`).json();
+const respond = (handler, directory) => (...[name, ...rest]) =>
+  async (app, ...noapp) => {
+    const base = app.runpath(app.config.location.server, directory);
+    const content = await base.join(`${name}.html`).text();
+    const toc = await base.join(`${name}.json`).json();
 
-  return handler({
-    content,
-    toc,
-  }, ...rest);
-};
+    return handler({content, toc}, ...rest)(app, ...noapp);
+  };
 
-const as_html = app => async ({content}, _, {status = Status.OK} = {}) =>
-  new Response(await app.render({body: content}), {
+const as_html = ({content}, _, {status = Status.OK, page} = {}) => async app =>
+  new Response(await app.render({body: content, page}), {
     status,
     headers: {...await app.headers(), "Content-Type": MediaType.TEXT_HTML},
 });
@@ -26,7 +25,6 @@ export default ({
 } = {}) => {
   const env = {};
   const re = new RegExp(`^.*.(?:${extension})$`, "u");
-  let compile;
 
   return {
     name: "primate:markdown",
@@ -36,7 +34,7 @@ export default ({
       return next(app);
     },
     register(app, next) {
-      app.register(extension, respond(handler ?? as_html(app), env.directory));
+      app.register(extension, respond(handler ?? as_html, env.directory));
       return next(app);
     },
     async compile(app, next) {
@@ -49,19 +47,19 @@ export default ({
       const target = app.runpath(location.server, env.directory);
       await target.file.create();
 
-      compile = await precompile({
-        app,
-        directory: env.directory,
-        options,
-      });
+      await Promise.all(components.map(async component => {
+        const filename = component.path;
+        const {content, toc} = compile(await component.text(), options);
 
-      await Promise.all(components.map(async component =>
-        compile(component.path, await component.file.read())));
+        const html = target.join(`${filename}.html`.replace(source, ""));
+        await html.directory.file.create();
+        await html.file.write(content);
+
+        const json = target.join(`${filename}.json`.replace(source, ""));
+        await json.file.write(stringify(toc));
+      }));
 
       return next(app);
-    },
-    async handle(request, next) {
-      return next({...request, markdown: {compile}});
     },
   };
 };
