f2549d410caa2106e9e1a3eedd8948ab6f4d429b
terrablue <102580937+terrablue@users.noreply.github.com>
2023-09-10 20:22:29 +0300

    store: add mongodb transactins

---
+++ packages/store/src/drivers/mongodb/Facade.js
@@ -0,0 +1,64 @@
+import {filter} from "runtime-compat/object";
+
+const toid = ({_id, ...rest}) => ({id: _id, ...rest});
+const to_id = ({id, ...rest}) => id === undefined ? rest : {_id: id, ...rest};
+const cid = criteria => criteria.id === undefined ? criteria : to_id(criteria);
+const null_to_set_unset = delta => {
+  const $set = filter(delta, ([, value]) => value !== null);
+  const $unset = filter(delta, ([, value]) => value === null);
+  return {$set, $unset};
+};
+
+export default class Facade {
+  schema = {
+    create: async _ => {
+      // noop
+    },
+    delete: async _ => {
+      // noop
+    },
+  };
+
+  constructor(connection, session) {
+    this.connection = connection;
+    this.session = session;
+  }
+
+  #by(name) {
+    return this.connection.collection(name);
+  }
+
+  get #options() {
+    return {session: this.session};
+  }
+
+  async find(name, criteria = {}) {
+    return (await this.#by(name).find(cid(criteria), this.#options).toArray())
+      .map(document => toid(document));
+  }
+
+  async count(name, criteria = {}) {
+    return this.#by(name).count(criteria, this.#options);
+  }
+
+  async get(name, primary, value) {
+    const result = await this.#by(name).findOne({_id: value}, this.#options);
+    return result === null ? undefined : toid(result);
+  }
+
+  async insert(name, primary, document) {
+    await this.#by(name).insertOne(document, this.#options);
+    return toid(document);
+  }
+
+  async update(name, criteria = {}, delta = {}) {
+    return (await this.#by(name)
+      .updateMany(to_id(criteria), null_to_set_unset(delta)), this.#options)
+      .modifiedCount;
+  }
+
+  async delete(name, criteria = {}) {
+    return (await this.#by(name).deleteMany(cid(criteria), this.#options))
+      .deletedCount;
+  }
+}
--- packages/store/src/drivers/mongodb/driver.js
+++ packages/store/src/drivers/mongodb/driver.js
@@ -2,17 +2,8 @@ import {filter} from "runtime-compat/object";
 import {ident} from "../base.js";
 import {peers} from "../common/exports.js";
 import depend from "../../depend.js";
-
-const toid = ({_id, ...rest}) => ({id: _id, ...rest});
-const to_id = ({id, ...rest}) => id === undefined ? rest : {_id: id, ...rest};
-
-const cid = criteria => criteria.id === undefined ? criteria : to_id(criteria);
-
-const null_to_set_unset = delta => {
-  const $set = filter(delta, ([, value]) => value !== null);
-  const $unset = filter(delta, ([, value]) => value === null);
-  return {$set, $unset};
-};
+import wrap from "../../wrap.js";
+import Facade from "./Facade.js";
 
 const name = "mongodb";
 const dependencies = ["mongodb"];
@@ -32,81 +23,60 @@ export default ({
     ObjectId,
     Decimal128,
   }] = await depend(on, `store:${name}`);
-  const url = `mongodb://${host}:${port}`;
-  const connection = new MongoClient(url);
-  await connection.connect();
-  const client = connection.db(db);
+  const url = `mongodb://${host}:${port}?replicaSet=rs0&directConnection=true`;
+  const client = new MongoClient(url);
+  await client.connect();
+
+  const types = {
+    primary: {
+      validate(value) {
+        /* TODO: check that has valid objectid form */
+        if (typeof value === "string") {
+          return value;
+        }
+        throw new Error(`\`${value}\` is not a valid primary key value`);
+      },
+      in(value) {
+        return new ObjectId(value);
+      },
+      out(value) {
+        return value.toString();
+      },
+    },
+    object: ident,
+    number: ident,
+    bigint: {
+      in(value) {
+        return new Decimal128(value.toString());
+      },
+      out(value) {
+        return BigInt(value.toString());
+      },
+    },
+    boolean: ident,
+    date: ident,
+    string: ident,
+  };
 
   return {
     name,
-    client,
-    async start() {
-      // noop
-    },
-    async rollback() {
-      // noop
-    },
-    async commit() {
-      // noop
-    },
-    async end() {
-      // noop
-    },
-    types: {
-      primary: {
-        validate(value) {
-          /* TODO: check that has valid objectid form */
-          if (typeof value === "string") {
-            return value;
-          }
-          throw new Error(`\`${value}\` is not a valid primary key value`);
-        },
-        in(value) {
-          return new ObjectId(value);
-        },
-        out(value) {
-          return value.toString();
-        },
-      },
-      object: ident,
-      number: ident,
-      bigint: {
-        in(value) {
-          return new Decimal128(value.toString());
-        },
-        out(value) {
-          return BigInt(value.toString());
-        },
-      },
-      boolean: ident,
-      date: ident,
-      string: ident,
-    },
-    with(name) {
-      return client.collection(name);
-    },
-    async find(collection, criteria = {}) {
-      const result = await this.with(collection).find(cid(criteria)).toArray();
-      return result.map(document => toid(document));
-    },
-    count(collection, criteria = {}) {
-      return this.with(collection).count(criteria);
-    },
-    async get(collection, primary, value) {
-      const result = await this.with(collection).findOne({_id: value});
-      return result === null ? undefined : toid(result);
-    },
-    async insert(collection, primary, document) {
-      await this.with(collection).insertOne(document);
-      return toid(document);
-    },
-    async update(collection, criteria = {}, delta = {}) {
-      return (await this.with(collection).updateMany(to_id(criteria),
-        null_to_set_unset(delta))).modifiedCount;
-    },
-    async delete(collection, criteria = {}) {
-      return (await this.with(collection).deleteMany(cid(criteria)))
-        .deletedCount;
+    types,
+    async transact(stores) {
+      return async (others, next) => {
+        const session = client.startSession();
+        const facade = new Facade(client.db(db), session);
+
+        try {
+          return await session.withTransaction(async () => {
+            const response = await next([...others, ...stores.map(([_, store]) =>
+              [_, wrap(store, facade, types)]),
+            ]);
+            return response;
+          });
+        } finally {
+          session.endSession();
+        }
+      };
     },
   };
 };
