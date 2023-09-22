b59a82aa664e861ed48404b1e43ee117d87baae9
terrablue <102580937+terrablue@users.noreply.github.com>
2023-09-22 21:39:43 +0300

    website: make mobile friendly

---
+++ packages/website/components/priss/Header.svelte
@@ -0,0 +1,62 @@
+<script>
+  import {onMount} from "svelte";
+
+  import Icons from "./Icons.svelte";
+  import Icon from "./Icon.svelte";
+
+  let highlight = _ => "";
+  let colorscheme;
+
+  const part = link => link.split("/")[1];
+
+  onMount(async () => {
+    colorscheme = (await import("./localStorage.js")).default;
+    highlight = link =>
+      part(link) === part(document.location.pathname) ? "active" : "";
+  });
+
+  export let app, title;
+  const {theme} = app;
+
+  const toggleColorScheme = () =>
+    colorscheme.update(value => value === "dark" ? "light" : "dark");
+</script>
+<svelte:head>
+  <title>Primate - {title}</title>
+</svelte:head>
+<Icons />
+<header>
+  <a class="home" href="/">
+    <img src="/logo.svg" />
+    <span class="title">{app.title}</span>
+  </a>
+
+  <div class="search"></div>
+
+  <ul class="navbar">
+    {#each theme.navbar as {link, label}}
+    <li>
+      <a href="{link}" class="{highlight(link)}">{label}</a>
+    </li>
+    {/each}
+
+    <div class="divider" />
+
+    <button class="ic" on:click={toggleColorScheme}>
+      <Icon name={$colorscheme === "dark" ? "sun" : "moon"} />
+    </button>
+
+    <a class="ic" href="{theme.chat}">
+      <Icon name="chat" />
+    </a>
+
+    <a class="ic" href="https://x.com/{theme.x}">
+      <Icon name="x" />
+    </a>
+
+    <a class="ic" href="https://github.com/{theme.github}">
+      <Icon name="github" />
+    </a>
+  </ul>
+
+</header>
