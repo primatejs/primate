a719f6fb4227a250543dcabc2a1f77ba7edcce5d
terrablue <102580937+terrablue@users.noreply.github.com>
2023-11-07 18:57:23 +0200

    binding/go: construct Request struct dynamically

--- packages/binding/src/bindings/go/module.js
+++ packages/binding/src/bindings/go/module.js
@@ -1,12 +1,14 @@
 import { Path } from "rcompat/fs";
+import { upperfirst } from "rcompat/string";
+import { keymap } from "rcompat/object";
 import { execSync } from "node:child_process";
 const default_extension = "go";
 const command = "go";
-const run = name => `${command} build -o ${name.base}wasm ${name}`;
+const run = name => `${command} build -o ${name.base}wasm ${name} request.go`;
 const name = "go";
 const routes_re = /func (?<route>Get|Post|Put|Delete)/gu;
 const add_setter = route =>
-  `js.Global().Set("${route}", js.FuncOf(primate.MakeRequest(${route})))`;
+  `js.Global().Set("${route}", js.FuncOf(make_request(${route})));`;
 
 const make_route = route =>
   `  ${route.toLowerCase()}(request) {
@@ -37,17 +39,17 @@ import "syscall/js"
 ${code}
 // {{{ wrapper postfix
 func main() {
-  c := make(chan bool)
+  c := make(chan bool);
   ${routes.map(route => add_setter(route)).join("\n  ")}
-  <-c
+  <-c;
 }
 // }}} end`;
 
 const get_routes = code => [...code.matchAll(routes_re)]
   .map(({ groups: { route } }) => route);
 
-const create_meta_files = async directory => {
-  const meta = { mod: "go.mod", sum: "go.sum" };
+const create_meta_files = async (directory, types) => {
+  const meta = { mod: "go.mod", sum: "go.sum", request: "request.go" };
   const base = new Path(import.meta.url).up(1);
 
   if (!await directory.join(meta.mod).exists()) {
@@ -55,6 +57,17 @@ const create_meta_files = async directory => {
     await directory.join(meta.mod).write(await base.join(meta.mod).text());
     // copy go.sum file
     await directory.join(meta.sum).write(await base.join(meta.sum).text());
+    // copy request.go file
+    const struct = Object.keys(keymap(types,
+      type => `Get${upperfirst(type)} func(string) string`))[0];
+    const init = `func(property string) string {
+      return properties[property].(string);
+    },`;
+    await directory.join(meta.request).write((await base.join(meta.request)
+      .text())
+      .replace("%%DISPATCH_STRUCT%%", _ => struct)
+      .replace("%%DISPATCH_INIT%%", _ => init),
+    );
   }
 };
 
@@ -65,8 +78,8 @@ export default ({ extension = default_extension } = {}) => {
     name: `primate:${name}`,
     async init(app, next) {
       app.register(extension, {
-        route: async (cwd, file) => {
-          await create_meta_files(cwd);
+        route: async (cwd, file, types) => {
+          await create_meta_files(cwd, types);
 
           const path = cwd.join(file);
           const code = await path.text();
---
+++ packages/binding/src/bindings/go/request.go
@@ -0,0 +1,89 @@
+package main
+
+import "syscall/js"
+import "encoding/json"
+
+type t_request func(Request) interface{} 
+type t_response func(js.Value, []js.Value) interface{}
+
+type Dispatchable struct {
+  Get func(string) string
+  GetAll func() map[string]interface{}
+  %%DISPATCH_STRUCT%%
+}
+
+type URL struct {
+  Href string
+  Origin string
+  Protocol string
+  Username string
+  Password string
+  Host string
+  Hostname string
+  Port string
+  Pathname string
+  Search string
+  SearchParams map[string]interface{}
+  Hash string
+}
+
+type Request struct {
+  Url URL
+  Body Dispatchable
+  Path Dispatchable
+  Query Dispatchable
+  Cookies Dispatchable
+  Headers Dispatchable
+}
+
+func make_url(request js.Value) URL {
+  url := request.Get("url");
+  search_params := make(map[string]interface{});
+  json.Unmarshal([]byte(request.Get("search_params").String()), &search_params);
+
+  return URL{
+    url.Get("href").String(),
+    url.Get("origin").String(),
+    url.Get("protocol").String(),
+    url.Get("username").String(),
+    url.Get("password").String(),
+    url.Get("host").String(),
+    url.Get("hostname").String(),
+    url.Get("port").String(),
+    url.Get("pathname").String(),
+    url.Get("search").String(),
+    search_params,
+    url.Get("hash").String(),
+  };
+}
+
+func make_dispatchable(key string, request js.Value) Dispatchable {
+  properties := make(map[string]interface{});
+  json.Unmarshal([]byte(request.Get(key).String()), &properties);
+
+  return Dispatchable{
+    func(property string) string {
+      return properties[property].(string);
+    },
+    func() map[string]interface{} {
+      return properties;
+    },
+    %%DISPATCH_INIT%%
+  };
+}
+
+func make_request(route t_request) t_response {
+  return func(this js.Value, args[] js.Value) interface{} {
+    request := args[0];
+    go_request := Request{
+      make_url(request),
+      make_dispatchable("body", request),
+      make_dispatchable("path", request),
+      make_dispatchable("query", request),
+      make_dispatchable("cookies", request),
+      make_dispatchable("headers", request),
+    };
+
+    return route(go_request);
+  };
+}
