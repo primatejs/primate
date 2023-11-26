4ac4a8622b3a70bda4955020372eefb6eb32cb33
terrablue <102580937+terrablue@users.noreply.github.com>
2023-11-26 20:57:42 +0200

    core/view: use app.extensions

--- packages/primate/src/handlers/view.js
+++ packages/primate/src/handlers/view.js
@@ -2,6 +2,6 @@ import errors from "../errors.js";
 
 export default (name, props, options) => async (app, ...rest) => {
   const extension = name.slice(name.lastIndexOf("."));
-  return app.handlers[extension]?.(name, props, options)(app, ...rest)
+  return app.extensions[extension]?.handle(name, props, options)(app, ...rest)
     ?? errors.NoHandlerForExtension.throw(extension, name);
 };
