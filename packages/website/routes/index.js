e83f960e5d7e312368104346472e0ac58fe69a77
terrablue <102580937+terrablue@users.noreply.github.com>
2024-01-24 20:41:32 +0200

    fix homepage

--- packages/website/routes/index.js
+++ packages/website/routes/index.js
@@ -1,8 +1,6 @@
 import { from } from "rcompat/object";
 import { view } from "primate";
 
-const example_files = ["backend", "frontend", "runtime", "i18n"];
-
 export default {
   async get(request) {
     const { env, config } = request;
@@ -10,12 +8,11 @@ export default {
 
     const base = await env.runpath(server, config.root, "examples");
 
-    const props = { 
-      app: request.config, 
-      examples: from((await Promise.all(example_files)).map(async section =>
+    const examples = from(await Promise.all(["backend", "frontend", "runtime", "i18n"]
+      .map(async section =>
        [section, await (base.join(`${section}.md.html`)).text()]
-      )),
-    };
+      )));
+    const props = { app: request.config, examples };
     const options = { placeholders: request.placeholders };
 
     return view("Homepage.svelte", props, options);
