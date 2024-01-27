11eb6c2ebfa3ca18e80750c15a9e7e17a22133e8
terrablue <102580937+terrablue@users.noreply.github.com>
2024-01-27 17:20:48 +0200

    initial typescript editor support

--- packages/primate/src/handlers/view.js
+++ packages/primate/src/handlers/view.js
@@ -1,11 +1,5 @@
 import errors from "../errors.js";
 
-/**
- * @param {string} name - component filename
- * @param {object} props - component props
- * @param {object} options - response options
- * @return {function(app: any, ...rest: any): Response } - rendering function
- */
 export default (name, props, options) => async (app, ...rest) => {
   const extension = name.slice(name.lastIndexOf("."));
   return app.extensions[extension]?.handle(name, props, options)(app, ...rest)
--- packages/primate/types/index.d.ts
+++ packages/primate/types/index.d.ts
@@ -1,4 +1,64 @@
-declare module 'primate' {
-  export function view(name: string, props: object, options: object): 
-    (app: any, ...rest: any) => Response
+declare module "primate" {
+  type App = any;
+
+  interface MinOptions {
+    status: number,
+  }
+  
+  interface ErrorOptions extends MinOptions {
+    page: string,
+  }
+
+  interface Options extends ErrorOptions {
+    placeholders: {},
+    headers: Headers | {},
+  }
+
+  type Dispatcher = {
+    get(property: string): string,
+  }
+
+  type RequestFacade = {
+    body: {}
+    path: Dispatcher,
+    query: Dispatcher,
+    cookies: Dispatcher,
+    headers: Dispatcher,
+    original: Request,
+  }
+
+  type ResponseFn = (app: App, ...rest: any) => Response;
+  type ResponseFacade = 
+    string 
+  | object
+  | URL
+  | Blob
+  | ReadableStream
+  | Response
+  | ResponseFn;
+
+  type RouteFunction = (request: RequestFacade) => ResponseFacade;
+
+  type Streamable = ReadableStream;
+
+  export type Route = {
+    get?: RouteFunction,
+    post?: RouteFunction,
+    put?: RouteFunction,
+    delete?: RouteFunction,
+  }
+
+  export function text(body: string, options?: MinOptions): ResponseFn;
+
+  export function json(body: {}, options?: MinOptions): ResponseFn;
+
+  export function stream(body: Streamable, options?: MinOptions): ResponseFn;
+
+  export function redirect(location: string, options?: MinOptions): ResponseFn;
+
+  export function html(name: string, options?: MinOptions): ResponseFn;
+
+  export function view(name: string, props: {}, options?: Options): ResponseFn;
+
+  export function error(body: string, options?: ErrorOptions): ResponseFn;
 }
