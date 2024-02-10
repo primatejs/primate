832d47031120eae65faff8fcdf64802f3fe79427
terrablue <102580937+terrablue@users.noreply.github.com>
2024-02-10 18:54:05 +0200

    website: add link to marko

--- packages/website/primate.config.js
+++ packages/website/primate.config.js
@@ -158,6 +158,7 @@ export default {
             "HTMX",
             "Handlebars",
             "Markdown",
+            "Marko",
             { heading: "Data" },
             "Types",
             "Store",
