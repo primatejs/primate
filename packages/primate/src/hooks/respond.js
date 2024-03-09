655fc13eb8329466cddcf9ad8c3a0d87e3d0ea45
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-09 13:24:27 +0200

    update deps, use rcompat/object.proper

--- packages/primate/src/hooks/respond.js
+++ packages/primate/src/hooks/respond.js
@@ -1,12 +1,12 @@
 import { Blob, s_streamable } from "rcompat/fs";
 import { URL, Response } from "rcompat/http";
 import { identity } from "rcompat/function";
+import o from "rcompat/object";
 import { text, json, stream, redirect } from "primate";
 import errors from "../errors.js";
 
 const not_found = value => errors.InvalidBodyReturned.throw(value);
 const is_text = value => typeof value === "string";
-const is_non_null_object = value => typeof value === "object" && value !== null;
 const is_instance = of => value => value instanceof of;
 const is_response = is_instance(globalThis.Response);
 const is_fake_response = is_instance(Response);
@@ -19,7 +19,7 @@ const guesses = [
   [is_streamable, value => stream(value.stream())],
   [is_instance(ReadableStream), stream],
   [value => is_response(value) || is_fake_response(value), value => _ => value],
-  [is_non_null_object, json],
+  [o.proper, json],
   [is_text, text],
   [not_found, identity],
 ];
