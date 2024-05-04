45f542e572c54d2195af01a297f0182d1c30dd60
terrablue <102580937+terrablue@users.noreply.github.com>
2024-05-04 22:58:32 +0200

    @primate/native: initial commit

---
+++ packages/native/src/module.js
@@ -0,0 +1,46 @@
+import gi from "node-gtk";
+
+const Gtk = gi.require("Gtk", "3.0");
+const WebKit2 = gi.require("WebKit2");
+
+gi.startLoop();
+
+Gtk.init();
+
+const window = new Gtk.Window({ type : Gtk.WindowType.TOPLEVEL });
+const webView = new WebKit2.WebView();
+const scrollWindow = new Gtk.ScrolledWindow({});
+
+const hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+const vbox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
+
+scrollWindow.add(webView);
+
+vbox.packStart(hbox, false, true, 0);
+vbox.packStart(scrollWindow, true, true, 0);
+
+window.setDefaultSize(1024, 720);
+window.setResizable(true);
+window.add(vbox);
+
+window.on("show", () => Gtk.main());
+window.on("destroy", () => Gtk.mainQuit());
+window.on("delete-event", () => false);
+
+export default () => {
+  return {
+    name: "primate:native",
+    async init(app, next) {
+      // open first argument or Google
+
+      setTimeout(() => {
+        const { host, port }= app.get("http");
+        const url = `http://${host}:${port}`;
+        webView.loadUri(url);
+        window.showAll();
+      }, 5000);
+
+      return next(app);
+    },
+  };
+};
