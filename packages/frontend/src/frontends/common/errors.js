d9c63f105a99e277c7176715d87b8a42b70cfae0
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-15 14:44:36 +0200

    frontend: remove errors.json

--- packages/frontend/src/frontends/common/errors.js
+++ packages/frontend/src/frontends/common/errors.js
@@ -1,6 +1,16 @@
-import { File } from "rcompat/fs";
 import { Logger } from "primate";
 
-const json = await new File(import.meta.url).up(1).join("errors.json").json();
+const errors = {
+  ErrorInComponent: {
+    message: "error in component {0}",
+    fix: "fix previous error in {1}",
+    level: "Error",
+  },
+  MissingComponent: {
+    message: "missing component {0}",
+    fix: "create {1} or remove route function",
+    level: "Error",
+  },
+};
 
-export default Logger.err(json.errors, json.module);
+export default Logger.err(errors, "@primate/frontend");
