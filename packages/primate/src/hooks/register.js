2071f9e4c3e9cd7ac0ab5a806bced9d1483a57c5
terrablue <102580937+terrablue@users.noreply.github.com>
2024-07-16 15:07:10 +0200

    fix missing client/static regression

--- packages/primate/src/hooks/register.js
+++ packages/primate/src/hooks/register.js
@@ -24,6 +24,9 @@ const post = async app => {
     // copy static files to build/server/static
     await app.stage(_static, FS.File.join(location.server, location.static));
 
+    // copy static files to build/client/static
+    await app.stage(_static, FS.File.join(location.client, location.static));
+
     // copy static files to build/static
     await app.stage(_static, FS.File.join(location.static));
 
