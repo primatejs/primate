43f59f12969d6e4d9d488bbe3342236c67e16b01
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-04 12:53:15 +0200

    create @primate/handlebars

--- packages/frontend/src/server/handlebars/public/runtime.js
+++
@@ -1,8 +0,0 @@
-import default_extension from "#handlebars/extension";
-import name from "#handlebars/name";
-import serve from "#handlebars/serve";
-
-export default ({ extension = default_extension } = {}) => ({
-  name: `primate:${name}`,
-  serve: serve(extension),
-});
