2c0a6c38fbd4c32f2a9142046dd5a22c26ba04ba
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-04 14:22:39 +0200

    recreate @primate/vue

--- packages/frontend/src/server/vue/public/runtime.js
+++
@@ -1,8 +0,0 @@
-import default_extension from "#vue/extension";
-import name from "#vue/name";
-import serve from "#vue/serve";
-
-export default ({ extension = default_extension } = {}) => ({
-  name: `primate:${name}`,
-  serve: serve(extension),
-});
--- packages/vue/src/public/runtime.js
+++ packages/vue/src/public/runtime.js
@@ -1,10 +1,8 @@
-import build from "#vue/build";
 import default_extension from "#vue/extension";
-import name from "#vue/name";
+import pkgname from "#name";
 import serve from "#vue/serve";
 
 export default ({ extension = default_extension } = {}) => ({
-  name: `primate:${name}`,
-  build: build(extension),
+  name: pkgname,
   serve: serve(extension),
 });
