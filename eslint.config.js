034ffee26b8bcbe260faff4b8c5d637519c359b4
terrablue <102580937+terrablue@users.noreply.github.com>
2024-12-03 00:48:31 +0100

    upgrade rcompat packages, use eslint rules from rcompat

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,42 @@
-export {default} from "maximin";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "*.config.js",
+      "scripts/*.js",
+      "**/lib/**/*",
+      "**/node_modules/**/*",
+      "packages/webview/src/bun/worker/init.js"
+    ]
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unnecessary-condition": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true
+        }
+      ]
+    }
+  },
+);
