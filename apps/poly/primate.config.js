39ec1fa5f08dea50f290c0bdda114c4e14aeaf82
terrablue <102580937+terrablue@users.noreply.github.com>
2025-04-04 21:08:54 +0200

    add apps/poly

---
+++ apps/poly/primate.config.js
@@ -0,0 +1,6 @@
+import poly from "@primate/poly";
+import i18n from "@primate/i18n";
+
+export default {
+  modules: [poly(), i18n()],
+}
---
+++ apps/poly/routes/index.js
@@ -0,0 +1,12 @@
+import view from "primate/handler/view";
+
+const posts = [{
+  id: 1,
+  title: "First post",
+}];
+
+export default {
+  get() {
+    return view("index.poly", { posts });
+  }
+}
